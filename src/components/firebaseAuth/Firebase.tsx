630aea48b6294d099c991adc2bd7a06c9e46aa63
Your Name <your_email@example.com>
2025-01-11 13:05:57 +0530

    bug fixed

--- src/components/firebaseAuth/Firebase.tsx
+++ src/components/firebaseAuth/Firebase.tsx
@@ -1,6 +1,6 @@
 // firebase.js
 import { initializeApp } from "firebase/app";
-import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { getAuth, GoogleAuthProvider, } from "firebase/auth";
 // import { getFirestore } from "firebase/firestore";
 import { getDatabase } from "firebase/database";
 
